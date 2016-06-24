1009b0e0cfacc5723ad02f7c2b3f6f6349fa042b
Istvan Kovacs <istvan.kovacs@mattakis.com>
2016-06-24 10:10:34 +0200

    Init project

---
+++ sdk/src/androidTest/java/com/androidlib/test/FunctionalTests.java
@@ -0,0 +1,19 @@
+package com.androidlib.test;
+
+import android.test.AndroidTestCase;
+
+import org.junit.Test;
+
+public class FunctionalTests extends AndroidTestCase {
+
+    private static final String TAG = FunctionalTests.class.getSimpleName();
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Test
+    public void test() {
+    }
+
+}
